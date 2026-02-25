f4e32efbd2e78e84c695547550fc35c09b660277
AndrewM <amichel13@toromail.csudh.edu>
2026-02-25 02:03:05 +0000

    Lab #7

---
+++ amichel7.py
@@ -0,0 +1,26 @@
+#Andrew Michel
+#Lab #7
+#CSC 321
+
+x = 2
+y = 3.5
+z = "world"
+
+if x > 10:
+    print("X is greater than 10")
+elif x < 10:
+    print("X is less than 10")
+else:
+    print("X is equal to 10")
+
+if y > 5.5:
+    print("y is greater than 5.5")
+elif x < 5.5:
+    print("y is less than 5.5")
+else:
+    print("y is equal to 5.5")
+
+if z == "Hello":
+    print("z is equal to Hello")
+else:
+    print("z does not equal Hello")
